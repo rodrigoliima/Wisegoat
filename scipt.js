2628477227bf74da9b62d19e0fc0cf74e5c661c3
Rodrigo Lima <97465641+rodrigoliima@users.noreply.github.com>
2022-01-29 12:35:42 -0300

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,24 @@
+const phases = [
+    "No meio da dificuldade encontra-se a oportunidade.",
+    "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo.",
+    "De todas as atividades humanas, preocupar é a menos produtiva.",
+    "Existem brilhos que em meio as trevas se ofuscam, mas existem trevas que em meio a luz se desfaz.",
+    "Os que não se lembram do seu passado estão condenados a repití-lo.",
+    "Felicidade não é um destino, é uma viagem.",
+    "Deus perdoa os nossos pecados, mas não nos poupa das consequências."
+    ]
+
+const phase = Math.floor(Math.random()*phases.length);
+
+console.log(phases[phase]);
+
+
+
+
+
+
+
+
+
+
+
